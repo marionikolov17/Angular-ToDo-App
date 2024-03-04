e9373d8794b142b6ba6637439b32a1212ec794fa
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-04 20:34:07 +0200

    Create API service

---
+++ client/src/app/api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor() { }
+
+  public getTasks() : void {
+
+  }
+
+  public createTask() : void {
+
+  }
+
+  public updateTask() : void {
+
+  }
+
+  public removeTask() : void {
+    
+  }
+}
