2d46ae4040cf0e33833a5704db018147c376bce7
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-04 13:01:20 +0200

    Add todo-options component

---
+++ client/src/app/todo-options/todo-options.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-options',
+  templateUrl: './todo-options.component.html',
+  styleUrls: ['./todo-options.component.css']
+})
+export class TodoOptionsComponent {
+
+}
