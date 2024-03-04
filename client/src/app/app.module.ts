a5e2250f3c1d9dbedaacb6ec8722fade547e1f93
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-04 20:34:30 +0200

    Import API Service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CreateTodoComponent } from './create-todo/create-todo.component';
 import { TodoOptionsComponent } from './todo-options/todo-options.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { ApiService } from './api.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,7 @@ import { TodoListComponent } from './todo-list/todo-list.component';
     NgbModule,
     FontAwesomeModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
