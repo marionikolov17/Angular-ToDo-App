42f56c1c664dbbb9dfb7682d8070e79b6df5e757
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-04 13:01:32 +0200

    Import components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CreateTodoComponent } from './create-todo/create-todo.component';
+import { TodoOptionsComponent } from './todo-options/todo-options.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateTodoComponent,
+    TodoOptionsComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
