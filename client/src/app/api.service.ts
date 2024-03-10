373daa351dbbace7e9cabeed5e9328ede46be1d3
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-10 19:50:24 +0200

    Rename folder Types

--- client/src/app/api.service.ts
+++ client/src/app/api.service.ts
@@ -1,40 +1,34 @@
 import { Injectable } from '@angular/core';
-import { ToDo } from './models/todo';
+import { ToDo } from './types/todo';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
   private tasks: ToDo[] = [
     {
       id: 1,
-      text: "Some task 1",
-      dueDate: "25th Mar 2024",
-      createdAt: "17th Mar 2024"
+      text: 'Some task 1',
+      dueDate: '25th Mar 2024',
+      createdAt: '17th Mar 2024',
     },
     {
       id: 2,
-      text: "Some task 2",
-      dueDate: "25th Jun 2024",
-      createdAt: "17th Apr 2024"
+      text: 'Some task 2',
+      dueDate: '25th Jun 2024',
+      createdAt: '17th Apr 2024',
     },
-  ]
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  public getTasks() : any {
+  public getTasks(): any {
     return this.tasks;
   }
 
-  public createTask() : void {
+  public createTask(): void {}
 
-  }
+  public updateTask(): void {}
 
-  public updateTask() : void {
-
-  }
-
-  public removeTask() : void {
-
-  }
+  public removeTask(): void {}
 }
--- client/src/app/todo-list/todo-list.component.ts
+++ client/src/app/todo-list/todo-list.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { faCheckSquare, faHourglass, faInfoCircle, faPencilAlt, faSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCheckSquare,
+  faHourglass,
+  faInfoCircle,
+  faPencilAlt,
+  faSquare,
+  faTrash,
+} from '@fortawesome/free-solid-svg-icons';
 import { ApiService } from '../api.service';
-import { ToDo } from '../models/todo';
+import { ToDo } from '../types/todo';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent implements OnInit {
   /* Icons */
@@ -15,13 +22,13 @@ export class TodoListComponent implements OnInit {
   faPencil = faPencilAlt;
   faTrash = faTrash;
   faInfoCircle = faInfoCircle;
-  faHourglass = faHourglass
-  tasks: ToDo[] = []
+  faHourglass = faHourglass;
+  tasks: ToDo[] = [];
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
-      this.tasks = this.apiService.getTasks();
-      console.log(this.tasks);
+    this.tasks = this.apiService.getTasks();
+    console.log(this.tasks);
   }
 }
